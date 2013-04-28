66c57d64c700588fc4dfc08bf25fe632e47ebf2c
Simonas Kazlauskas <git@kazlauskas.me>
2013-04-28 20:25:32 +0300

    Fix documentation

--- src/Hakyll/Web/Template/Blaze.hs
+++ src/Hakyll/Web/Template/Blaze.hs
@@ -19,10 +19,10 @@ import Text.Blaze.Html.Renderer.String (renderHtml)
 type Template m a = (String -> m String) -> Item a -> m Html
 
 
-applyTemplate :: Template Compiler String -- | Blaze template
-              -> Context String           -- | Hakyll context
-              -> Item String              -- | The item
-              -> Compiler (Item String)   -- | Resulting HTML
+applyTemplate :: Template Compiler String -- ^ Blaze template
+              -> Context String           -- ^ Hakyll context
+              -> Item String              -- ^ The item
+              -> Compiler (Item String)   -- ^ Resulting HTML
 applyTemplate tpl ctx item =
     tpl ctx' item
     >>= return . renderHtml
@@ -32,11 +32,11 @@ applyTemplate tpl ctx item =
     ctx' key = unContext (ctx `mappend` missingField) key item
 
 
-applyTemplateListWith :: String              -- | String to join template with
-                      -> Template Compiler String -- | Blaze template
-                      -> Context String           -- | Hakyll context
-                      -> [Item String]            -- | List of items
-                      -> Compiler String          -- | Resulting HTML
+applyTemplateListWith :: String              -- ^ String to join template with
+                      -> Template Compiler String -- ^ Blaze template
+                      -> Context String           -- ^ Hakyll context
+                      -> [Item String]            -- ^ List of items
+                      -> Compiler String          -- ^ Resulting HTML
 applyTemplateListWith delimiter tpl ctx items =
     mapM (applyTemplate tpl ctx) items
     >>= return . intercalate delimiter . map itemBody
@@ -48,13 +48,13 @@ applyTemplateList = applyTemplateListWith ""
 toHtml, safeToHtml :: String -> Html
 -- | Synonym for blaze's toHtml. The only difference being that input string is
 -- enforced to String type. This is necessary for easy use of this function
--- with OverloadedStrings. Otherwise compiler can't infer the appropriate type
--- to use and fails.
+-- with -XOverloadedStrings. Otherwise compiler can't infer the appropriate
+-- type to use and fails.
 toHtml = H.toHtml
 
 -- | Synonym for blaze's preEscapedToHtml. The only difference being that input
 -- string is enforced to String type. This is necessary for easy use of this
--- function with OverloadedStrings. Otherwise compiler can't infer the
+-- function with -XOverloadedStrings. Otherwise compiler can't infer the
 -- appropriate type to use and fails.
 -- Also safeToHtml sounds better than preEscapedToHtml
 safeToHtml = H.preEscapedToHtml
